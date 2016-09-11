39a3f7a1ca895498b00eedee2f1e8c64ae932136
chiyingleung1 <chiying.leung@gmail.com>
2016-09-11 15:16:48 +0000

    Added photo model with caption field only

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911121917) do
+ActiveRecord::Schema.define(version: 20160911150814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: true do |t|
+    t.text     "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "players", force: true do |t|
     t.string   "name"
